c293d7d1b3f89797aeb0b63aa332cccd339e5e59
Yaroslav Galitsyn <galicin20045@gmail.com>
2024-03-25 13:25:34 +0700

    Edited entrypoint.sh
    Signed-off-by: Yaroslav Galitsyn <galicin20045@gmail.com>

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-certbot certonly --standalone -d pcost.tech --email yaroslav@pcost.tech -n --agree-tos --expand
+certbot certonly --standalone -d pcost.tech,api.pcost.tech --email yaroslav@pcost.tech -n --agree-tos --expand
 /usr/sbin/nginx -g "daemon off;"
